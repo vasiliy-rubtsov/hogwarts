398ce8f780ece4235bb893561e0cf6d26c02a052
Vasiliy Rubtsov <vasiliy.rubtsov@rtmis.ru>
2025-03-04 02:42:10 +0300

    Тестирование с использованием TestRestTemplate Исправления по результатам CR

--- src/test/java/ru/hogwarts/school/controller/FacultyControllerV1Test.java
+++ src/test/java/ru/hogwarts/school/controller/FacultyControllerV1Test.java
@@ -10,8 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
 
-import java.util.Collection;
+import java.util.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -28,12 +29,21 @@ public class FacultyControllerV1Test {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @Autowired
+    StudentController studentController;
+
+    /**
+     * Создался ли экземпляр контроллера со всеми включенными в него зависимостями?
+     */
     @Test
     @Order(1)
     public void contextLoads() {
         Assertions.assertThat(controller).isNotNull();
     }
 
+    /**
+     * Добавление нового факультета
+     */
     @Test
     @Order(2)
     public void addFaculty() {
@@ -43,6 +53,9 @@ public class FacultyControllerV1Test {
         Assertions.assertThat(id).isNotNull();
     }
 
+    /**
+     * Получение списка факультетов
+     */
     @Test
     @Order(3)
     public void getAllFaculty() {
@@ -50,10 +63,45 @@ public class FacultyControllerV1Test {
         Assertions.assertThat(response).isNotNull();
     }
 
+    /**
+     * Получение факултьтета по его ID
+     */
     @Test
     @Order(4)
     public void getFaculty() {
         Faculty response = testRestTemplate.getForObject("http://localhost:" + port + "/faculty/" + id, Faculty.class);
         Assertions.assertThat(response.getName()).isEqualTo("Тестовый факультет");
     }
+
+    /**
+     * Получение факультета по фрагменту цвета или названия
+     */
+    @Test
+    @Order(5)
+    public void findGlobal() {
+        // Проверяем поиск по фрагменту названия
+        ArrayList<LinkedHashMap<String, ?>> response = testRestTemplate.getForObject("http://localhost:" + port + "/faculty/findGlobal?str=тест", ArrayList.class);
+        Assertions.assertThat(response.size()).isGreaterThan(0);
+        LinkedHashMap<String, ?> firstElement = response.get(0);
+        Assertions.assertThat(firstElement.get("name")).isEqualTo("Тестовый факультет");
+
+        // Проверяем поиск по фрагменту цвета
+        response = testRestTemplate.getForObject("http://localhost:" + port + "/faculty/findGlobal?str=тест", ArrayList.class);
+        Assertions.assertThat(response.size()).isGreaterThan(0);
+        firstElement = response.get(0);
+        Assertions.assertThat(firstElement.get("name")).isEqualTo("Тестовый факультет");
+    }
+
+    /**
+     * Получение факультета по точному значению цвета
+     */
+    @Test
+    @Order(6)
+    public void findByColor() {
+        ArrayList<LinkedHashMap<String, ?>> response = testRestTemplate.getForObject("http://localhost:" + port + "/faculty/findByColor?color=Фиолетовый", ArrayList.class);
+        Assertions.assertThat(response.size()).isGreaterThan(0);
+        LinkedHashMap<String, ?> firstElement = response.get(0);
+        Assertions.assertThat(firstElement.get("name")).isEqualTo("Тестовый факультет");
+    }
+
 }
