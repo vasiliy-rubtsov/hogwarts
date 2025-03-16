5e8a7c6587b45f30abc885a636c142685b074878
Vasiliy Rubtsov <vasiliy.rubtsov@rtmis.ru>
2025-03-16 12:57:57 +0300

    ДЗ 4.2 Управление схемами баз данных Доработки по результатам CR

--- scripts421.sql
+++ scripts421.sql
@@ -4,9 +4,12 @@ ALTER TABLE student ADD CONSTRAINT student_age CHECK (age >= 16);
 -- Добавляем конроль заполнения имен студентов
 ALTER TABLE student ALTER COLUMN name SET NOT NULL;
 
-
 -- Добавляем уникальность для имен студентов
 ALTER TABLE student ADD CONSTRAINT name_unique UNIQUE(name);
 
 -- Устанавливаем для возраста студента значение по умолчанию 20
 ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
+
+-- Добавляем уникальность сочетанию название и цвет факультета
+ALTER TABLE faculty ADD CONSTRAINT name_color UNIQUE(name, color);
+
